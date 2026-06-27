1544276b701368ef3cdd2b939d4f801ca5439b67
haiqaldanielafiq <haiqaldanielafiq@gmail.com>
2026-06-28 00:44:46 +0800

    Add files via upload

---
+++ data/questions.ts
@@ -0,0 +1,14 @@
+export const questions=[
+{
+id:1,
+question:"100 sen bersamaan...",
+choices:["RM1.00","RM10.00","10 sen","RM0.10"],
+answer:"RM1.00"
+},
+{
+id:2,
+question:"RM12.30 + RM5.40 = ?",
+choices:["RM17.70","RM18.70","RM16.70","RM15.70"],
+answer:"RM17.70"
+}
+]
